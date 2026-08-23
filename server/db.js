5ec2fca2bf17cf5d087f6a7f12965b5b7d9c394d
romeesa07 <romeeromeesa7@gmai.com>
2026-08-23 06:47:09 -0400

    configure MongoDB connection

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:BU8txrOW8jxXYbTAmwMPLqTE@172.21.126.215:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
