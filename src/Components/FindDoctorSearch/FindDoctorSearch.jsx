844fa7c8c958ebe7ddd8281238c8c8dc93304d24
romeesa07 <romeeromeesa7@gmail.com>
2026-08-24 05:51:29 -0400

    Create FindDoctorSearch component

---
+++ src/Components/FindDoctorSearch/FindDoctorSearch.jsx
@@ -0,0 +1,85 @@
+
+import React, { useState } from 'react';
+import './FindDoctorSearch.css';
+
+const specialities = [
+  'Dentist',
+  'Gynecologist/obstetrician',
+  'General Physician',
+  'Dermatologist',
+  'Ear-nose-throat (ent) Specialist',
+  'Homeopath',
+  'Ayurveda'
+];
+
+const FindDoctorSearch = () => {
+  const [showResults, setShowResults] = useState(false);
+  const [searchDoctor, setSearchDoctor] = useState('');
+
+  const handleDoctorSelect = (speciality) => {
+    setSearchDoctor(speciality);
+    setShowResults(false);
+  };
+
+  return (
+    <div className="find-doctor">
+
+      <h1>Find a doctor</h1>
+
+      <p className="search-subtitle">
+        Search by specialty
+      </p>
+
+      <div className="doctor-search-box">
+
+        <div className="search-input-wrapper">
+
+          <input
+            type="text"
+            className="search-doctor-input-box"
+            placeholder="Search doctors, clinics, hospitals, etc."
+            value={searchDoctor}
+            onFocus={() => setShowResults(true)}
+            onChange={(e) => setSearchDoctor(e.target.value)}
+          />
+
+          <img
+            className="find-icon"
+            src="/images/search.svg"
+            alt="Search"
+          />
+
+        </div>
+
+        {showResults && (
+          <div className="search-doctor-input-results">
+
+            {specialities.map((speciality) => (
+              <div
+                className="search-doctor-result-item"
+                key={speciality}
+                onMouseDown={() => handleDoctorSelect(speciality)}
+              >
+                <img
+                  src="/images/search.svg"
+                  alt=""
+                />
+
+                <span>{speciality}</span>
+
+                <span className="speciality-label">
+                  SPECIALITY
+                </span>
+              </div>
+            ))}
+
+          </div>
+        )}
+
+      </div>
+
+    </div>
+  );
+};
+
+export default FindDoctorSearch;
