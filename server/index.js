9e1f85ce6677b1cf299f4b483401bad71d91c0c8
romeesa07 <romeeromeesa7@gmal.com>
2026-08-24 16:40:54 -0400

    Configure production build

--- server/index.js
+++ server/index.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 const connectToMongo = require('./db');
+const path = require('path');
+
 const app = express();
-
-
-app.set('view engine','ejs')
-app.use(express.static('public'))
-
 const PORT = process.env.PORT || 8181;
-
 
 // Middleware
 app.use(express.json());
@@ -21,13 +16,15 @@ connectToMongo();
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Serve production React build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// React Router fallback
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
-
-  // Start the server
+// Start the server
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
