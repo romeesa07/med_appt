4f13cbbe800ac95d620316586fe434ffdac8b59f
romeesa07 <romeeromeesa7@gmail.com>
2026-08-24 04:31:22 -0400

    Integrate instant consultation component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Landing_Page from "./Components/Landing_Page/Landing_Page";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
+import InstantConsultation from "./Components/InstantConsultation/InstantConsultation";
 
 function App() {
   return (
@@ -22,6 +23,8 @@ function App() {
 
           {/* Login */}
           <Route path="/login" element={<Login />} />
+
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
         </Routes>
 
       </BrowserRouter>
